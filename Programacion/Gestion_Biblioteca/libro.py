76728d33ffe886d1ba3e4fb07b587e6c0e211560
Alejandra Pinto <konshuelo@hotmail.com>
2025-09-05 19:42:45 -0400

    Update libro.py

--- Programacion/Gestion_Biblioteca/libro.py
+++ Programacion/Gestion_Biblioteca/libro.py
@@ -1,37 +1,45 @@
 class Libro:
-    def _init__(self, nombre, genero, isbn, autor, estado= false):
+    #metodo constructor
+    def __init__(self,nombre,genero,isbn,autor,estado=False):
+        self.__nombre= nombre
+        self.__genero= genero
+        self.__isbn= isbn
+        self.__autor= autor
+        self.__estado= estado
+    
+    #metodo str
+    def __str__(self):
+        return f"Nombre: {self.__nombre} - Genero: {self.__genero} - ISBN: {self.__isbn} - Autor: {self.__autor} - Estado: {self.__estado}"
+    #Metodos Destructor
+    def __del__(self):
+        print (f"El libro {self.__nombre} ha sido eliminado")
+
+    # metodos get y set
+    def getNombre(self):
+        return self.__nombre
+    def getGenero(self):
+        return self.__genero
+    def getIsbn(self):
+        return self.__isbn
+    def getAutor(self):
+        return self.__autor
+    def getEstado(self):
+        return self.__estado
+    
+    def setNombre(self,nombre):
         self.__nombre = nombre
-        self.__genero = genero
-        self.__isbn = isbn
+    def setAutor(self,autor):
         self.__autor = autor
+    def setGenero(self,genero):
+        self.__genero = genero
+    def setIsbn(self,isbn):
+        self.__isbn = isbn
+    def setEstado(self,estado):
         self.__estado = estado
+        
 
-def__ str__(self):
-    return f"Nombre: {self.__nombre}, Genero: {self.__genero}, ISBN: {self.__isbn}, Autor: {self.__autor}, Estado: {'Prestado' if self.__estado else 'Disponible'}"
+it = Libro("IT","Terror",12312411124,"Stiphen King")
+print(it)
+it.setAutor("Stephen King")
+print(it.getAutor())
 
-def get_nombre(self):
-    return self.__nombre
-
-def get_genero(self):
-    return self.__genero
-def get_isbn(self):
-    return self.__isbn
-def get_autor(self):
-    return self.__autor
-def get_estado(self):
-    return self.__estado
-
-
-def set_Nombre(self, nombre):
-    self.__nombre = nombre
-def set_Autor(self, autor):
-    self.__autor = autor
-
-def set_Genero(self, genero):
-    self.__genero = genero
-
-def set_Isbn(self, isbn):
-    self.__isbn = isbn
-
-def set_Estado(self, estado):
-    self.__estado = estado
